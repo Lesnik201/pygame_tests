e190dfccfd173ff750926dae8c395186bf0e2354
Lesnik201 <viktorviktorovich1678@gmail.com>
2023-03-28 16:31:24 +1000

    Убрал звезду

--- Игра_шарики.py
+++ Игра_шарики.py
@@ -19,9 +19,6 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 ball_x, ball_y, ball_r = 0, 0, 0
 score = 0
-star_coords = [randint(0, 900), randint(100, 800)]
-diff_star_x = 5
-diff_star_y = -5
 
 
 def new_ball():
@@ -47,7 +44,7 @@ def click(event):
             score_choice = 30
         else:
             score_choice = 10
-        print_score(score_choice, center=(ball_x, ball_y-ball_r), ball=True, font_size=40)
+        print_score(score_choice, center=(ball_x, ball_y - ball_r), ball=True, font_size=40)
     score += score_choice
 
 
@@ -62,13 +59,6 @@ def print_score(num_score, center=(1100, 50), ball=False, font_size=30):
     screen.blit(text, place)
 
 
-def star(star_x, star_y):
-    polygon(screen, 'yellow', ((star_x + 10, star_y), (star_x + 20, star_y - 30), (star_x, star_y - 50),
-                               (star_x + 30, star_y - 50), (star_x + 40, star_y - 95), (star_x + 50, star_y - 50),
-                               (star_x + 80, star_y - 50), (star_x + 60, star_y - 30), (star_x + 70, star_y),
-                               (star_x + 40, star_y - 20)))
-
-
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -81,14 +71,6 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
-
-    if 1120 <= star_coords[0] or star_coords[0] <= 0:
-        diff_star_x = 0 - diff_star_x
-    if 900 <= star_coords[1] or star_coords[1] <= 95:
-        diff_star_y = 0 - diff_star_y
-    star_coords[0] += diff_star_x
-    star_coords[1] += diff_star_y
-    star(star_coords[0], star_coords[1])
 
     new_ball()
     print_score(str(score))
